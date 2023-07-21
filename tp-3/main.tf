c011292a79fe49ba3fd1cb47991555bfed6e7a75
sadofrazer <sadofrazer@yahoo.fr>
2023-07-21 10:29:48 +0200

    add tp3 on variables and attributes reference

---
+++ tp-3/main.tf
@@ -0,0 +1,70 @@
+data "aws_ami" "my_ami" {
+  most_recent = true
+  owners = ["amazon"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.my_ami.id
+  instance_type = var.instance_type
+  key_name = "fsa-kp-ib"
+  tags = {
+    Name = "${var.owner}-ec2-test"
+    Formation= "terraform"
+  }
+}
+
+
+resource "aws_eip" "my_eip" {
+  instance = aws_instance.web.id
+  domain   = "vpc"
+}
+
+
+resource "aws_security_group" "my_sg" {
+  name        = "allow_http_s"
+  description = "Allow HTTP/HTTPS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.owner}-sg-allow_http_s"
+  }
+}
+
+
+output "my-eip" {
+  value = aws_eip.my_eip.public_ip
+}
